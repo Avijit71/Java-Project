a51b3eb8365f5644732d8bbf672ea4abe930df76
Avijit71 <pandeyavijit924@gmail.com>
2025-08-01 00:20:47 +0530

    Java Project added

---
+++ Coding/src/com/coding/Six.java
@@ -0,0 +1,23 @@
+package com.coding;
+
+public class Six {
+
+	 static String rollno1="Dilip";
+	 static String rollno2="Abhijeet";
+	 static String rollno3="Sameer";
+	 
+	 static void printStudentDetail()
+	 {
+		    System.out.println(Six.rollno1);
+			System.out.println(Six.rollno2);
+			System.out.println(Six.rollno3);
+		}
+	 }
+	 
+	 
+		
+
+	
+
+
+
---
+++ Coding/src/com/coding/Test2.java
@@ -0,0 +1,15 @@
+package com.coding;
+class Test2{
+	public static void main (String[]args)
+	{
+		//Six student=new Six();
+//		System.out.println(Six.rollno1);
+//		System.out.println(Six.rollno2);
+//		System.out.println(Six.rollno3);
+		
+		
+		Six.printStudentDetail();
+	}
+}
+
+
